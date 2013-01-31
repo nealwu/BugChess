340293944f8d4f69477bd0541aa8a7d09ae46662
Neal Wu <neal@nealwu.com>
2013-01-31 00:23:04 -0500

    Remove console.log

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var app = require('http').createServer(handler),
 app.listen(80);
 
 function handler(req, res) {
-    console.log(req + ' ' + res);
     fs.readFile(__dirname + '/index.html',
     function(err, data) {
         if (err) {
