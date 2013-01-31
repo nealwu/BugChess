ccd0dc66c47c0a661e30643a1846b3fa5997e0d1
Neal Wu <neal@nealwu.com>
2013-01-30 23:59:10 -0500

    Change port from 80 to 1000

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = require('http').createServer(handler),
     io = require('socket.io').listen(app),
     fs = require('fs');
 
-app.listen(80);
+app.listen(1000);
 
 function handler(req, res) {
     fs.readFile(__dirname + '/index.html',
