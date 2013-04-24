c21bb9b6adac11690a988abf88d5437875ab8d1e
Neal Wu <neal@nealwu.com>
2013-04-24 01:24:30 -0400

    Remove jade and ejs from app.js

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ var express = require('express'),
 app.configure(function() {
     app.set('port', process.env.PORT || 80);
     app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
+    // app.set('view engine', 'jade');
     app.use(express.favicon());
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
-
-    app.engine('html', require('ejs').__express);
 });
 
 app.configure('development', function() {
