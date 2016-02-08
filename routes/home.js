827798c3859c81a178413efd0428825b4cd814c4
Neal Wu <neal@nealwu.com>
2016-02-07 18:29:04 -0800

    Send the user to every jade template

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 var privileges = require('./middlewares/privileges');
 
 router.get('/', privileges.ensureAuthenticated, function(req, res) {
-  res.render('home');
+  res.render('home', { user: req.user });
 });
 
 module.exports = router;
